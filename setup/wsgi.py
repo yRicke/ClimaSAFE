bee75617687a87ae13d17b96389c9144f165f1eb
unknown <henriquevieiraperes@gmail.com>
2026-05-30 13:34:47 -0300

    vercel configuration

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
+
+def _run_migrations_once_on_vercel() -> None:
+    if os.getenv("VERCEL") != "1":
+        return
+
+    marker = Path("/tmp/.django_migrated")
+    if marker.exists():
+        return
+
+    call_command("migrate", interactive=False, run_syncdb=True, verbosity=0)
+    marker.touch()
+
+
+_run_migrations_once_on_vercel()
 application = get_wsgi_application()
